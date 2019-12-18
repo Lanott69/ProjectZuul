cdf09cd26e91ae882b613d578fac967981e55618
Lanott69 <59028281+Lanott69@users.noreply.github.com>
2019-12-18 23:07:19 +0100

    printlocationinfo toegevoegd

--- zuul-bad/Game.java
+++ zuul-bad/Game.java
@@ -97,7 +97,25 @@ public class Game
         }
         System.out.println();
     }
-
+    
+    private void printLocationInfo()
+    {
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exists: ");
+        if(currentRoom.northExit != null) {
+            System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null) {
+            System.out.print("east ");
+        }
+        if(currentRoom.southExit != null) {
+            System.out.print("south ");
+        }
+        if(currentRoom.westExit != null) {
+            System.out.print("west ");
+        }
+        System.out.println();
+    }
     /**
      * Given a command, process (that is: execute) the command.
      * @param command The command to be processed.
