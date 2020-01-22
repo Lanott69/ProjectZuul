8a1d0529fffe5d0a9e92fa6a5bab3e4469e9d4b2
SophieB28980 <59033907+SophieB28980@users.noreply.github.com>
2020-01-22 14:35:09 +0100

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private Room currentRoom;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        
+        
+    }
+    
+  
+    
+}
